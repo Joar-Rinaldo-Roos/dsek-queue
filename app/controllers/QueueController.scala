53a8f025628120de1f8fc92dbf6e5e2f640186fa
Adam Månsson <adam.s.mansson@gmail.com>
2022-02-14 21:46:44 +0100

    Change to a user-based approach

--- app/controllers/QueueController.scala
+++ app/controllers/QueueController.scala
@@ -1,33 +1,52 @@
 package controllers
 
-import models.{QueueEntry, dtoEntry}
+import models._
 import play.api.mvc._
 
 import javax.inject._
 import scala.collection.mutable
 import play.api.libs.json._
 
+import scala.collection.mutable.ArrayBuffer
+
 @Singleton
 class QueueController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  private val queue = new mutable.ArrayBuffer[QueueEntry]()
-  queue += QueueEntry("cola")
-  queue += QueueEntry("macka")
+  private val users = new mutable.ArrayBuffer[QueueUser]()
+  private val allOrders = new mutable.ArrayBuffer[QueueOrder]()
+  // private val queue = new mutable.ArrayBuffer[QueueEntry]()
 
-  implicit val entryJson: OFormat[QueueEntry] = Json.format[QueueEntry]
-  implicit val dtoEntryJson: OFormat[dtoEntry] = Json.format[dtoEntry]
+  implicit val userJson: OFormat[QueueUser] = Json.format[QueueUser]
+  implicit val orderJson: OFormat[QueueOrder] = Json.format[QueueOrder]
+  implicit val dtoOrderJson: OFormat[dtoOrder] = Json.format[dtoOrder]
 
-  // curl localhost:9000/queue
-  def readAll: Action[AnyContent] = Action {
-    if (queue.isEmpty) {
-      NoContent
+  // curl localhost:9000
+  def index: Action[AnyContent] = Action { implicit request =>
+    if (getUserByCookie(request.cookies.get("userId")).isDefined) {
+      Ok("You're back!")
     } else {
-      Ok(Json.toJson(queue))
+      val aUserId = QueueUser.getCounter
+      users += QueueUser(aUserId)
+
+      Ok("Welcome!").withCookies(Cookie("userId", aUserId.toString, Option(180)))
     }
   }
 
-  // curl localhost:9000/queue/1
-  def read(aId: Long): Action[AnyContent] = Action {
-    val foundEntry = queue.find(_.id == aId)
+  def testCookies: Action[AnyContent] = Action { implicit request =>
+    Ok(request.cookies.get("userId").get.value)
+  }
+
+  // curl localhost:9000/queue/read
+  def readAll(): Action[AnyContent] = Action {
+    if (users.isEmpty) {
+      NoContent
+    } else {
+      Ok(Json.toJson(users))
+    }
+  }
+
+  // curl localhost:9000/queue/0
+  def read(aId: Int): Action[AnyContent] = Action {
+    val foundEntry = users.find(_.userId == aId)
     if (foundEntry.isDefined) {
       Ok(Json.toJson(foundEntry.get))
     } else {
@@ -35,9 +54,19 @@ class QueueController @Inject()(val controllerComponents: ControllerComponents) 
     }
   }
 
+  def readOwnOrder(): Action[AnyContent] = Action { implicit request =>
+    val aUser = getUserByCookie(request.cookies.get("userId"))
+    if (aUser.isDefined) {
+      Accepted(Json.toJson(aUser.get.viewOrders))
+    } else {
+      NotFound
+    }
+  }
+
+  /*
   // curl -X PUT localhost:9000/queue/done/0
-  def markDone(aId: Long): Action[AnyContent] = Action {
-    val foundEntry = queue.find(_.id == aId)
+  def markDone(aId: Int): Action[AnyContent] = Action {
+    val foundEntry = queue.find(_.orderId == aId)
     if (foundEntry.isDefined) {
       val asDone = foundEntry.get.copy(isDone = true)
       queue -= foundEntry.get
@@ -49,25 +78,52 @@ class QueueController @Inject()(val controllerComponents: ControllerComponents) 
   }
 
   // curl -X DELETE localhost:9000/queue/delete
-  def deleteAllDone: Action[AnyContent] = Action {
+  def deleteAllDone(): Action[AnyContent] = Action {
     queue.filterInPlace(_.isDone == false)
     Accepted
   }
 
+   */
+
   // curl -v -d '{"order": "some new order"}' -H 'Content-Type: application/json' -X POST localhost:9000/queue/add
-  def add: Action[AnyContent] = Action { implicit request =>
+  def add(): Action[AnyContent] = Action { implicit request =>
+    Ok("Adding")
+
+    /*
+
     val content = request.body
     val jsonObject = content.asJson
-    val aQueueEntry: Option[dtoEntry] = jsonObject.flatMap(Json.fromJson[dtoEntry](_).asOpt)
+    val aQueueEntry: Option[dtoOrder] = jsonObject.flatMap(Json.fromJson[dtoOrder](_).asOpt)
 
     aQueueEntry match {
-      case Some(newEntry) =>
-        val toBeAdded = QueueEntry(newEntry.order)
-        queue += toBeAdded
-        Created(Json.toJson(toBeAdded))
+      case Some(aOrder) =>
+        getUserByCookie(request.cookies.get("userId")) match {
+          case Some(aUser) =>
+            val toBeAdded = QueueOrder(aUser, aOrder.content)
 
-      case None =>
-        BadRequest
+            aUser.addOrder(toBeAdded)
+            allOrders += toBeAdded
+
+            Created(Json.toJson(toBeAdded))
+
+          case None => BadRequest
+        }
+      case None => BadRequest
+    }
+
+     */
+  }
+
+  def getUserByCookie(aCookie: Option[Cookie]): Option[QueueUser] = {
+    if (aCookie.isDefined) {
+      val aUser = users.find(_.userId == aCookie.get.value.toInt)
+      if (aUser.isDefined) {
+        Some(aUser.get)
+      } else {
+        None
+      }
+    } else {
+      None
     }
   }
 }
--- app/models/QueueEntry.scala
+++ app/models/QueueEntry.scala
@@ -1,14 +1,34 @@
 package models
+import scala.collection.mutable
 
-case class QueueEntry(id: Long, order: String, isDone: Boolean)
+case class QueueOrder(orderId: Int, owner: QueueUser, content: String, isDone: Boolean)
 
-case class dtoEntry(order: String)
+case class dtoOrder(content: String)
 
-object QueueEntry {
+object QueueOrder {
   private var counter = 0
 
-  def apply(aOrder: String): QueueEntry = {
+  def apply(aOwner: QueueUser, aContent: String): QueueOrder = {
     counter += 1
-    new QueueEntry(counter - 1, aOrder, false)
+    new QueueOrder(counter - 1, aOwner, aContent, isDone = false)
+  }
+}
+
+case class QueueUser(userId: Int) {
+  private val orders = new mutable.ArrayBuffer[QueueOrder]()
+
+  def viewOrders: mutable.ArrayBuffer[QueueOrder] = orders
+
+  def addOrder(aOrder: QueueOrder): Unit = orders += aOrder
+}
+
+object QueueUser {
+  private var counter = 1000
+
+  def getCounter: Int = counter
+
+  def apply(aUserId: Int): QueueUser = {
+    counter += 1
+    new QueueUser(aUserId)
   }
 }
