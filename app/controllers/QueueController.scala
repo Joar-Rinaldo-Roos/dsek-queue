ca5239afbab46cda6504eab09acb641e3e941263
Adam Månsson <adam.s.mansson@gmail.com>
2022-02-14 10:12:08 +0100

    Add commands markDone, delete, add

--- app/controllers/QueueController.scala
+++ app/controllers/QueueController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.QueueEntry
+import models.{QueueEntry, dtoEntry}
 import play.api.mvc._
 
 import javax.inject._
@@ -9,12 +9,14 @@ import play.api.libs.json._
 
 @Singleton
 class QueueController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  private val queue = new mutable.ListBuffer[QueueEntry]()
+  private val queue = new mutable.ArrayBuffer[QueueEntry]()
   queue += QueueEntry("cola")
   queue += QueueEntry("macka")
 
-  implicit val queueJson: OFormat[QueueEntry] = Json.format[QueueEntry]
+  implicit val entryJson: OFormat[QueueEntry] = Json.format[QueueEntry]
+  implicit val dtoEntryJson: OFormat[dtoEntry] = Json.format[dtoEntry]
 
+  // curl localhost:9000/queue
   def readAll: Action[AnyContent] = Action {
     if (queue.isEmpty) {
       NoContent
@@ -23,23 +25,49 @@ class QueueController @Inject()(val controllerComponents: ControllerComponents) 
     }
   }
 
-  def readById(aId: Long): Action[AnyContent] = Action {
+  // curl localhost:9000/queue/1
+  def read(aId: Long): Action[AnyContent] = Action {
     val foundEntry = queue.find(_.id == aId)
     if (foundEntry.isDefined) {
-      Ok(Json.toJson(foundEntry))
+      Ok(Json.toJson(foundEntry.get))
     } else {
       NoContent
     }
   }
 
-  def delete(aId: Long): Action[AnyContent] = Action {
-
+  // curl -X PUT localhost:9000/queue/done/0
+  def markDone(aId: Long): Action[AnyContent] = Action {
+    val foundEntry = queue.find(_.id == aId)
+    if (foundEntry.isDefined) {
+      val asDone = foundEntry.get.copy(isDone = true)
+      queue -= foundEntry.get
+      queue += asDone
+      Accepted(Json.toJson(asDone))
+    } else {
+      NotFound
+    }
   }
 
+  // curl -X DELETE localhost:9000/queue/delete
+  def deleteAllDone: Action[AnyContent] = Action {
+    queue.filterInPlace(_.isDone == false)
+    Accepted
+  }
+
+  // curl -v -d '{"order": "some new order"}' -H 'Content-Type: application/json' -X POST localhost:9000/queue/add
   def add: Action[AnyContent] = Action { implicit request =>
     val content = request.body
     val jsonObject = content.asJson
-    val queueEntry = jsonObject.map(Json.fromJson[QueueEntry])
-  }
+    val aQueueEntry: Option[dtoEntry] = jsonObject.flatMap(Json.fromJson[dtoEntry](_).asOpt)
 
+    aQueueEntry match {
+      case Some(newEntry) =>
+        val toBeAdded = QueueEntry(newEntry.order)
+        queue += toBeAdded
+        Created(Json.toJson(toBeAdded))
+
+      case None =>
+        BadRequest
+    }
+  }
 }
--- app/models/QueueEntry.scala
+++ app/models/QueueEntry.scala
@@ -2,8 +2,10 @@ package models
 
 case class QueueEntry(id: Long, order: String, isDone: Boolean)
 
+case class dtoEntry(order: String)
+
 object QueueEntry {
-  private var counter = 0;
+  private var counter = 0
 
   def apply(aOrder: String): QueueEntry = {
     counter += 1
